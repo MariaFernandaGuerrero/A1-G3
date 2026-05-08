4413f5c3405beceae8cdbde1eabf6587ad68dff5
Maria Fernanda Guerrero <mafegusa13@gmail.com>
2026-05-08 07:09:25 -0500

    Screenshots for Block Diagram, EC signal and PSD

--- GNURadio/mpsk.py
+++ GNURadio/mpsk.py
@@ -6,42 +6,30 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Not titled yet
-# GNU Radio version: 3.10.1.1
-
-from packaging.version import Version as StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
+# GNU Radio version: v3.10.11.0-89-ga17f69e7
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio.filter import firdes
-import sip
 from gnuradio import blocks
 import numpy
 from gnuradio import fft
 from gnuradio.fft import window
 from gnuradio import filter
+from gnuradio.filter import firdes
 from gnuradio import gr
 import sys
 import signal
+from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 import math
 import mpsk_epy_block_0 as epy_block_0  # embedded python block
 import mpsk_epy_block_0_0 as epy_block_0_0  # embedded python block
+import sip
+import threading
 
 
-
-from gnuradio import qtgui
 
 class mpsk(gr.top_block, Qt.QWidget):
 
@@ -52,8 +40,8 @@ class mpsk(gr.top_block, Qt.QWidget):
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -66,15 +54,15 @@ class mpsk(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "mpsk")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "mpsk")
 
         try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+        self.flowgraph_started = threading.Event()
 
         ##################################################
         # Variables
@@ -87,10 +75,12 @@ class mpsk(gr.top_block, Qt.QWidget):
         self.Rs = Rs = Rb/Nb
         self.samp_rate = samp_rate = Rs*Sps
         self.N = N = 1024
+        self.Fc = Fc = 16000
 
         ##################################################
         # Blocks
         ##################################################
+
         self.Menu = Qt.QTabWidget()
         self.Menu_widget_0 = Qt.QWidget()
         self.Menu_layout_0 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.Menu_widget_0)
@@ -102,11 +92,16 @@ class mpsk(gr.top_block, Qt.QWidget):
         self.Menu_grid_layout_1 = Qt.QGridLayout()
         self.Menu_layout_1.addLayout(self.Menu_grid_layout_1)
         self.Menu.addTab(self.Menu_widget_1, 'Tab 1')
+        self.Menu_widget_2 = Qt.QWidget()
+        self.Menu_layout_2 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.Menu_widget_2)
+        self.Menu_grid_layout_2 = Qt.QGridLayout()
+        self.Menu_layout_2.addLayout(self.Menu_grid_layout_2)
+        self.Menu.addTab(self.Menu_widget_2, 'Tab 2')
         self.top_layout.addWidget(self.Menu)
         self.qtgui_vector_sink_f_0 = qtgui.vector_sink_f(
             N,
-            -samp_rate/2,
-            samp_rate/N,
+            (-samp_rate/2),
+            (samp_rate/N),
             "f",
             "Sx(f)",
             "PSD (Watts/Hz)",
@@ -114,12 +109,13 @@ class mpsk(gr.top_block, Qt.QWidget):
             None # parent
         )
         self.qtgui_vector_sink_f_0.set_update_time(0.10)
-        self.qtgui_vector_sink_f_0.set_y_axis(0, 1/Rb)
+        self.qtgui_vector_sink_f_0.set_y_axis(0, (1/Rb))
         self.qtgui_vector_sink_f_0.enable_autoscale(True)
         self.qtgui_vector_sink_f_0.enable_grid(False)
         self.qtgui_vector_sink_f_0.set_x_axis_units("Hz")
         self.qtgui_vector_sink_f_0.set_y_axis_units("Watss/Hz")
         self.qtgui_vector_sink_f_0.set_ref_level(0)
+
 
         labels = ['p4', '', '', '', '',
             '', '', '', '', '']
@@ -146,8 +142,8 @@ class mpsk(gr.top_block, Qt.QWidget):
         for c in range(0, 1):
             self.Menu_grid_layout_0.setColumnStretch(c, 1)
         self.qtgui_time_sink_x_0_1_0_0_0 = qtgui.time_sink_f(
-            int(32*Sps/Nb), #size
-            16000, #samp_rate
+            (int(32*Sps/Nb)), #size
+            Fc, #samp_rate
             "", #name
             1, #number of inputs
             None # parent
@@ -194,7 +190,7 @@ class mpsk(gr.top_block, Qt.QWidget):
         self._qtgui_time_sink_x_0_1_0_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_1_0_0_0.qwidget(), Qt.QWidget)
         self.Menu_layout_1.addWidget(self._qtgui_time_sink_x_0_1_0_0_0_win)
         self.qtgui_time_sink_x_0_1_0_0 = qtgui.time_sink_c(
-            int(32*Sps/Nb), #size
+            (int(32*Sps/Nb)), #size
             samp_rate, #samp_rate
             "", #name
             1, #number of inputs
@@ -245,7 +241,7 @@ class mpsk(gr.top_block, Qt.QWidget):
         self._qtgui_time_sink_x_0_1_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_1_0_0.qwidget(), Qt.QWidget)
         self.Menu_layout_0.addWidget(self._qtgui_time_sink_x_0_1_0_0_win)
         self.qtgui_time_sink_x_0_1_0 = qtgui.time_sink_c(
-            int(32/Nb), #size
+            (int(32/Nb)), #size
             Rs, #samp_rate
             "", #name
             1, #number of inputs
@@ -302,8 +298,8 @@ class mpsk(gr.top_block, Qt.QWidget):
             None # parent
         )
         self.qtgui_const_sink_x_0.set_update_time(0.10)
-        self.qtgui_const_sink_x_0.set_y_axis(-2, 2)
-        self.qtgui_const_sink_x_0.set_x_axis(-2, 2)
+        self.qtgui_const_sink_x_0.set_y_axis((-2), 2)
+        self.qtgui_const_sink_x_0.set_x_axis((-2), 2)
         self.qtgui_const_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
         self.qtgui_const_sink_x_0.enable_autoscale(False)
         self.qtgui_const_sink_x_0.enable_grid(False)
@@ -335,11 +331,11 @@ class mpsk(gr.top_block, Qt.QWidget):
             self.qtgui_const_sink_x_0.set_line_alpha(i, alphas[i])
 
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.qwidget(), Qt.QWidget)
-        self.Menu_layout_0.addWidget(self._qtgui_const_sink_x_0_win)
+        self.Menu_layout_2.addWidget(self._qtgui_const_sink_x_0_win)
         self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_ccc(Sps, h)
         self.interp_fir_filter_xxx_0.declare_sample_delay(0)
         self.fft_vxx_0 = fft.fft_vcc(N, True, [1]*N, True, 1)
-        self.epy_block_0_0 = epy_block_0_0.blk(M=M, A=1, fc=16000, fs=samp_rate, sps=Sps)
+        self.epy_block_0_0 = epy_block_0_0.blk(M=M, A=1, fc=Fc, fs=samp_rate, sps=Sps)
         self.epy_block_0 = epy_block_0.blk(N=N)
         self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, N)
         self.blocks_multiply_const_vxx_1 = blocks.multiply_const_vff([1/(N*samp_rate)]*N)
@@ -365,7 +361,7 @@ class mpsk(gr.top_block, Qt.QWidget):
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "mpsk")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "mpsk")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -394,7 +390,7 @@ class mpsk(gr.top_block, Qt.QWidget):
     def set_Rb(self, Rb):
         self.Rb = Rb
         self.set_Rs(self.Rb/self.Nb)
-        self.qtgui_vector_sink_f_0.set_y_axis(0, 1/self.Rb)
+        self.qtgui_vector_sink_f_0.set_y_axis(0, (1/self.Rb))
 
     def get_Nb(self):
         return self.Nb
@@ -427,7 +423,7 @@ class mpsk(gr.top_block, Qt.QWidget):
         self.blocks_multiply_const_vxx_1.set_k([1/(self.N*self.samp_rate)]*self.N)
         self.epy_block_0_0.fs = self.samp_rate
         self.qtgui_time_sink_x_0_1_0_0.set_samp_rate(self.samp_rate)
-        self.qtgui_vector_sink_f_0.set_x_axis(-self.samp_rate/2, self.samp_rate/self.N)
+        self.qtgui_vector_sink_f_0.set_x_axis((-self.samp_rate/2), (self.samp_rate/self.N))
 
     def get_N(self):
         return self.N
@@ -436,21 +432,28 @@ class mpsk(gr.top_block, Qt.QWidget):
         self.N = N
         self.blocks_multiply_const_vxx_1.set_k([1/(self.N*self.samp_rate)]*self.N)
         self.epy_block_0.N = self.N
-        self.qtgui_vector_sink_f_0.set_x_axis(-self.samp_rate/2, self.samp_rate/self.N)
+        self.fft_vxx_0.set_window([1]*self.N)
+        self.qtgui_vector_sink_f_0.set_x_axis((-self.samp_rate/2), (self.samp_rate/self.N))
+
+    def get_Fc(self):
+        return self.Fc
+
+    def set_Fc(self, Fc):
+        self.Fc = Fc
+        self.epy_block_0_0.fc = self.Fc
+        self.qtgui_time_sink_x_0_1_0_0_0.set_samp_rate(self.Fc)
 
 
 
 
 def main(top_block_cls=mpsk, options=None):
 
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
     tb = top_block_cls()
 
     tb.start()
+    tb.flowgraph_started.set()
 
     tb.show()
 
